a56e35b7226a2b58e3c8681ab88baf2974ff1108
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2016-12-10 22:19:32 +0900

    [yobidash] Add new functions: handle_dash_button(), sniff_handle_dash_button()

--- yobidash.py
+++ yobidash.py
@@ -4,6 +4,7 @@ import sys
 
 from scapy.all import *
 
+DASH_BUTTON_MAC_ADDR = 'SPECIFY MAC ADDR OF DASH BUTTON'
 
 def exit_if_user_run_this_script_as_general_user():
     if not os.getuid() == 0:
@@ -24,6 +25,19 @@ def sniff_found_dash_button(packet):
 
 def found_dash_button():
     print sniff(prn=sniff_found_dash_button, filter="arp", store=0, count=10)
+
+
+def sniff_handle_dash_button(packet):
+    arp_packet = packet[ARP]
+
+    if arp_packet.op == 1:
+        if arp_packet.hwdst == '00:00:00:00:00:00':
+            if arp_packet.hwsrc == DASH_BUTTON_MAC_ADDR:
+                print 'Dash button is pusshed!'
+
+
+def handle_dash_button():
+    print sniff(prn=sniff_handle_dash_button, filter="arp", store=0, count=0)
 
 
 if __name__ == '__main__':
