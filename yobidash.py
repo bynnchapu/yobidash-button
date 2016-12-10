619a7356fab6f0d4a0326eb5837c257d265b6e57
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2016-12-10 20:45:43 +0900

    [yobidash] Add new line to follow pep8.

--- yobidash.py
+++ yobidash.py
@@ -4,10 +4,12 @@ import sys
 
 from scapy.all import *
 
+
 def exit_if_user_run_this_script_as_general_user():
     if not os.getuid() == 0:
         print 'Error: You need root permission to run this script.'
         sys.exit(os.EX_NOPERM)
+
 
 def sniff_found_dash_button(packet):
     arp_packet = packet[ARP]
@@ -19,8 +21,10 @@ def sniff_found_dash_button(packet):
             print 'MAC Address of Dash button:' + arp_packet.hwsrc
             sys.exit(0)
 
+
 def found_dash_button():
     print sniff(prn=sniff_found_dash_button, filter="arp", store=0, count=10)
+
 
 if __name__ == '__main__':
     exit_if_user_run_this_script_as_general_user()
